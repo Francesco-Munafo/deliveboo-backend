6eda5b9ad681a52562e78e0b5bdc77486b054489
danielplebani <danielpleba03@gmail.com>
2023-12-05 17:36:51 +0100

    add types for every restaurant

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['ITALIANA', 'FRANCESE', 'GIAPPONESE', 'MESSICANA', 'CINESE', 'AMERICANA', 'INDIANA', 'MEDITERRANEA', 'VIETNAMITA', 'INGLESE'];
+
+        foreach ($types as $type) {
+            $new_type = new Type;
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($new_type->name, '-');
+            $new_type->save();
+        }
     }
 }
