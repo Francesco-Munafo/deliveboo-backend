feb54302b481f702ce8f5c62c111762fada58391
Francesco-Munafo <acquamarea1@gmail.com>
2023-12-05 12:11:33 +0100

    Add restaurant model and controller populated

---
+++ database/seeders/RestaurantSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Restaurant;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class RestaurantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $new_restaurant = new Restaurant();
+        $new_restaurant->name = 'Trattoria da Mario';
+        $new_restaurant->slug = Str::slug($new_restaurant->name, '-');
+        $new_restaurant->address = 'Via dei Mille, 78';
+        $new_restaurant->description = 'Il nostro ristorante è molto bello, siediti a mangia roba buona';
+        $new_restaurant->vat_number = '86334519757';
+        $new_restaurant->image = 'https://www.ristoranteallalega.com/photos/ristorante.jpg';
+
+        $new_restaurant->save();
+    }
+}
