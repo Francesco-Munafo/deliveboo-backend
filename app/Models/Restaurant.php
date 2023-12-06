a89449b60b0d55507ecacf3ca952f8f0a2837bc9
Francesco-Munafo <acquamarea1@gmail.com>
2023-12-06 11:17:06 +0100

    Add orders table and restaurants relationship

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
@@ -15,7 +16,7 @@ class Restaurant extends Model
 
     protected $datas = ['deleted_at'];
 
-    protected $fillable = ['name', 'slug', 'address', 'description', 'vat_number', 'image', 'type_id'];
+    protected $fillable = ['name', 'slug', 'address', 'description', 'vat_number', 'image', 'type_id', 'order_id'];
 
     public static function generateSlug($name)
     {
@@ -25,5 +26,10 @@ class Restaurant extends Model
     public function types(): BelongsToMany
     {
         return $this->belongsToMany(Type::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
     }
 }
